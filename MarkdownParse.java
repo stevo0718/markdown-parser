3c32e3f758e60dbd8f1e3d8dfcd3a8f414e60eb1
stevo0718 <jiw084@ucsd.edu>
2022-04-22 10:51:02 -0700

    Wrong output
    When the "[]"does not exist in the string, it still prints out whatever is in the "()".

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,14 +11,17 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        String subString = markdown.substring(currentIndex, markdown.length()-1);
         while(currentIndex < markdown.length()) {
+            if (subString.contains("[") 
+                && subString.contains("]")){
                     int openBracket = markdown.indexOf("[", currentIndex);
                     int closeBracket = markdown.indexOf("]", openBracket);
                     int openParen = markdown.indexOf("(", closeBracket);
                     int closeParen = markdown.indexOf(")", openParen);
                     toReturn.add(markdown.substring(openParen + 1, closeParen));
                     currentIndex = closeParen + 1;  
-            
+            }
         }
         return toReturn;
     }

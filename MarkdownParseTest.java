ce9cb54b0e21615c40cd3873e189f8b15eba7a71
stevo0718 <jiw084@ucsd.edu>
2022-04-28 09:41:29 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,7 +14,7 @@ public class MarkdownParseTest {
 
    @Test
    public void test() {
-    List<String> list = List.of("ilovemoney");
+    List<String> list = List.of("nooo");
     Path fileName = Path.of("test-file4.md");
     String content = "";
     ArrayList<String> links = null;
